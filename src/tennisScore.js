352b13316af8f222b21a713f6854d6e0061232d4
RashLop <lopezmejiarasshid@outlook.com>
2025-08-28 10:16:58 -0400

    refactoring: Ahora se encapsula los puntajes para solo mostrar el contador

--- src/tennisScore.js
+++ src/tennisScore.js
@@ -1,5 +1,22 @@
-function showScore(){
-    return "Love - Love"; 
+class TennisScorer {
+    constructor(){
+        this.player1Points = 0; 
+    }
+    showScore(){
+        if (this.player1Points === 1){
+            return "15 - Love"; 
+        }
+        if(this.player1Points === 2)
+        {
+            return "30 - Love"; 
+
+        }
+        return "Love - Love"; 
+}
+player1Score(){
+    this.player1Points++; 
+}
 }
 
-export default showScore; 
+
+export default TennisScorer; 
--- src/tennisscore.spec.js
+++ src/tennisscore.spec.js
@@ -1,8 +1,22 @@
-import showScore from "./tennisScore"
-
+import TennisScorer from "./tennisScore"
 describe("TennisScore",  () => {
-    it("game empieza con ambos en cero",() =>{
-        expect(showScore()).toEqual("Love - Love")
-    })
-})
 
+    it("game empieza con ambos en cero",() =>{
+        const scorer = new TennisScorer(); 
+        expect(scorer.showScore()).toEqual("Love - Love")
+        
+    }); 
+
+    it("Jugador 1 Marca un punto ---> 15  0  ",() =>{
+        const scorer = new TennisScorer(); 
+        scorer.player1Score(); 
+        expect(scorer.showScore()).toEqual("15 - Love")
+    }); 
+
+    it("Jugador 1 Marca un punto ---> 15  0  ",() =>{
+        const scorer = new TennisScorer(); 
+        scorer.player1Score(); 
+        scorer.player1Score();         
+        expect(scorer.showScore("30 - Love")).toEqual("30 - Love")
+    });
+}); 
